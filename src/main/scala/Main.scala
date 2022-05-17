cc266793ec5767a44e34d6f8e6a0c0e6ee515833
Sicelamandla Zikhali <ngelemarweapons@gmail.com>
2022-05-17 20:07:05 +0200

    Initial empty project

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,23 @@
+package com.maths.graphs
+
+import Main.default
+
+/**
+ * Graphs first plan:
+ * Create type of graph:
+ * Simple/unweighted: Connected, Disconnected, Complete,
+ *                    Cycle, Cyclic, Bipartite, k-Partite,
+ *                    Tree, Forest
+ * Weighted
+ * Directed
+ * Directed Weighted
+ * Print/Visualise Graph
+ * */
+
+object Main extends App {
+  def default(a: String): Unit = {
+    println(s"Hello the string is:\n$a")
+  }
+
+  default("Welcome to the introduction of this project.")
+}
